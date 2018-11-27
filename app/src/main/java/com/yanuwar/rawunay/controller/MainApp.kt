218711dd05a7eb1262cfaf43bd0ba7a11b22a3bc
zanuar <zanuar@skyshi.io>
2018-11-27 11:19:22 +0700

    - mvc architecture

--- app/src/main/java/com/yanuwar/rawunay/controller/MainApp.kt
+++ app/src/main/java/com/yanuwar/rawunay/controller/MainApp.kt
@@ -1,9 +1,13 @@
-package com.yanuwar.rawunay
+package com.yanuwar.rawunay.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.yanuwar.rawunay.R
+import com.yanuwar.rawunay.model.Board
 
 class MainApp : AppCompatActivity() {
+
+    val board = Board()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/yanuwar/rawunay/model/Cell.kt
@@ -0,0 +1,4 @@
+package com.yanuwar.rawunay.model
+
+class Cell {
+}
